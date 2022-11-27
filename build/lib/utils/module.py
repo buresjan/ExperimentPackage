3be22366ad15a4406a12ce03dc49c48287ef0d25
buresjan <bures.work@gmail.com>
2022-11-27 13:21:55 +0100

    Initial commit

---
+++ build/lib/utils/module.py
@@ -0,0 +1,5 @@
+def hello_world():
+    print('i am here')
+
+if __name__ == "__main__":
+    pass
---
+++ setup.py
@@ -0,0 +1,2 @@
+import setuptools
+setuptools.setup(name="utils", version='0.0.1', packages=["utils"])
